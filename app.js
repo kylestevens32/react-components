8726278533687b7c934babb2e6861aa239ce811d
Kyle Stevens <kyleastevens3@gmail.com>
2023-02-21 11:55:40 -0800

    Completed the react components mini exercise

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-// TODO
+const { useState } = React;
+
+const GroceryListItem = ({item}) => {
+  const [isAcquired, setIsAcquired] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const onListItemHover = (event) => {
+
+  }
+
+  const style = {
+    textDecoration: isAcquired ? 'line-through' : 'none',
+    fontWeight: isHovered ? 'bold' : 'normal'
+  };
+
+  return (
+    <li style={style} onClick={() => setIsAcquired(!isAcquired)} onMouseOver={() => setIsHovered(!isHovered)} onMouseOut={() => setIsHovered(!isHovered)}>
+      {item}
+    </li>
+  );
+}
+
+const GroceryList = (props) => (
+  <ul>
+    {props.items.map((item) => (
+      <GroceryListItem item={item} />
+    ))}
+  </ul>
+)
+
+const App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Squid', 'Pork', 'Cucumber', 'Beer', 'Wine']}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+// const root = createRoot(document.getElementById("app"))
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
